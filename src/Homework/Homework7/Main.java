03180d85e9c671992a1c00e7b32b3c5094d96875
Daniel Kubenko <8lastday@gmail.com>
2023-05-02 16:27:26 +0300

    homework7 Символи та строки

--- src/Homework/Homework7/Main.java
+++ src/Homework/Homework7/Main.java
@@ -1,4 +1,36 @@
 package Homework7;
 public class Main {
-
+    public static int findSymbolOccurance(String str, char symbol) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == symbol) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int findWordPosition(String source, String target) {
+        if (source == null || target == null) {
+            return -1;
+        }
+        if (source.contains(target)) {
+            return source.indexOf(target);
+        } else {
+            return -1;
+        }
+    }
+    public static String stringReverse(String str) {
+        if (str == null) {
+            return null;
+        }
+        StringBuilder reversed = new StringBuilder(str);
+        return reversed.reverse().toString();
+    }
+    public static boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+        String reversed = stringReverse(str);
+        return str.equals(reversed);
+    }
 }
